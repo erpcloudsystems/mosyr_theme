26f34c32fe6b7c4bd4c136ebd67e1ae586f99c64
MaiIsmail <mai.mq.1995@gmail.com>
2022-07-19 15:20:37 +0300

    undo changes

--- mosyr_theme/public/js/custome_js.js
+++ mosyr_theme/public/js/custome_js.js
@@ -9,15 +9,9 @@ $(document).ready(function () {
     element.closest('ul').parent().addClass("open")
 
     $(".menu-link").click(function (event) {
-        setTimeout(function () {
-            if ( frappe.get_route() == '' || frappe.get_route()[1] == 'Home') {
-                window.location.reload();
-            }
-        }, 100)
         set_active_page(event)
     });
     $("#logo").click(function (event) {
-        window.location.reload();
         set_active_page(event)
     });
 
