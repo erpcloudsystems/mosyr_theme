66cfed00c859e1c79c3604160745505925135e5d
zaqoutabed <zaqoutabed@gmail.com>
2022-08-04 19:44:28 +0300

    fix pages style

--- mosyr_theme/public/js/frappe/aside.js
+++ mosyr_theme/public/js/frappe/aside.js
@@ -4,16 +4,25 @@ frappe.provide('frappe.search');
 frappe.ui.toolbar.MosyrSidebar = class {
 	constructor() {
 		let sidebar_items = []
-        $('#mosyrsidebar').empty()
+		$('#mosyrsidebar').empty()
 		$('#mosyrsidebar').html(frappe.render_template("sidebar", {
 			navbar_settings: frappe.boot.navbar_settings,
 			side_items: frappe.boot.sidebar_items || []
 		}));
 
 		this.make();
+		this.bind_events()
 	}
 
 	make() { }
+	bind_events() {
+		$("#mosyrsidebar .menu-item > a.menu-link.menu-toggle").on('click', function (ev) {
+			ev.stopPropagation()
+			$(".menu-inner .menu-item").removeClass("open")
+			// $("mosyrsidebar ul.menu-inner > li.menu-item").removeClass("open")
+			$($(ev.target).parent()).addClass("open")
+		})
+	}
 };
 
 frappe.Application.prototype.make_nav_bar = function () {
