dabcb346c723240af2b21f48eb753c7259bb61a4
zaqoutabed <zaqoutabed@gmail.com>
2022-08-09 17:32:10 +0300

    remove tech support doctype

--- mosyr_theme/mosyr_theme/doctype/technical_support/technical_support.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Mai and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Technical support', {
-	// refresh: function(frm) {
-
-	// }
-});
