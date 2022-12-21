230de27b16ab1b1b35a670dad10c67afdadace65
zaqoutabed <zaqoutabed@gmail.com>
2022-12-21 20:08:57 +0300

    rebuild sidebar in patch

--- mosyr_theme/patches/update_sidebar_items.py
+++ mosyr_theme/patches/update_sidebar_items.py
@@ -1,4 +1,19 @@
 def execute():
-    from mosyr_theme.install import set_sidebar
+    import frappe
+    from mosyr_theme.install import side_bar_static
 
-    set_sidebar()
+    if not frappe.db.exists("DocType", "System Controller"):
+        return
+    sc = frappe.get_doc("System Controller")
+    sc.enable = 1
+    sc.show_sidebar = 1
+    sc.sidebar_labels = []
+    sc.sidebar_item = []
+
+    for label in side_bar_static["sidebar_labels"]:
+        sc.append("sidebar_labels", label)
+
+    for label in side_bar_static["sidebar_item"]:
+        sc.append("sidebar_item", label)
+    sc.save(ignore_permissions=True)
+    frappe.db.commit()
